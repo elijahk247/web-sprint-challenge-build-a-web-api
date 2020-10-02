bed83c8dc74fcc63336b99bcca223eec4e925305
Elijah Kim <elijahkim@Elijahs-MacBook-Pro.local>
2020-10-02 11:29:42 -0400

    Finished basic setup of the project

---
+++ api/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const server = express();
+
+server.use(express.json());
+
+// separate files for CRUD operations on these db helpers 
+const actionsRouter = require('./actionsRouter.js');
+const projectsRouter = require('./projectsRouter.js');
+
+// endpoints
+server.use('/api/actions', actionsRouter);
+server.use('/api/products', projectsRouter);
+
+// landing 
+server.get('/', (req, res) => {
+  res.status(200).json({ message: 'sprint project' });
+})
+
+module.exports = server;
