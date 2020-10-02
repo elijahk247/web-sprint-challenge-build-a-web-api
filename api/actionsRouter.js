6b90e51bc12d23f69fe921e5205c4580a7112e09
Elijah Kim <elijahkim@Elijahs-MacBook-Pro.local>
2020-10-02 11:50:18 -0400

    Finished get request for action

--- api/actionsRouter.js
+++ api/actionsRouter.js
@@ -1,4 +1,20 @@
 const express = require('express')
-const router = express();
+const router = express.Router();
+
+const Actions = require('../data/helpers/actionModel');
+
+router.get('/:id', (req ,res) => {
+  Actions.get(req.params.id)
+    .then(action => {
+      if(action != 0) {
+        res.status(200).json(action);
+      } else {
+        res.status(404).json({ message: 'Action at given index does not exist' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'Error retrieving action' });
+    })
+})
 
 module.exports = router;
--- api/projectsRouter.js
+++ api/projectsRouter.js
@@ -1,4 +1,6 @@
 const express = require('express')
-const router = express();
+const router = express.Router();
+
+//const Projects = require('../data/helpers/projectModel.js');
 
 module.exports = router;
